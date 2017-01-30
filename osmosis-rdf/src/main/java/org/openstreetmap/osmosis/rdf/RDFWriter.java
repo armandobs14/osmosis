7c996ef7c397509a90b3d910d65b122b3382e174
williamsla <williamsalcantara@gmail.com>
2017-01-30 19:09:16 -0300

    handling tags of OSM features for each Node

--- osmosis-rdf/src/main/java/org/openstreetmap/osmosis/rdf/RDFWriter.java
+++ osmosis-rdf/src/main/java/org/openstreetmap/osmosis/rdf/RDFWriter.java
@@ -1,79 +1,119 @@
 package org.openstreetmap.osmosis.rdf;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
 import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer;
 import org.openstreetmap.osmosis.core.domain.v0_6.*;
 import org.openstreetmap.osmosis.core.task.v0_6.Sink;
-import org.openstreetmap.osmosis.core.util.LazyHashMap;
-
-import java.util.*;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.logging.Logger;
+import java.util.List;
+import org.bson.Document;
+import org.apache.commons.lang3.text.WordUtils;
 
 /**
  * Created by Williams on 27/01/17.
  */
 public class RDFWriter implements Sink {
 
-    private final String ONTO_OSM = "http://linkn.com.br/onto/osm/";
+    private final String OSM = "http://linkn.com.br/onto/osm/";
+    private final String RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 
-    Logger logger = Logger.getLogger(RDFWriter.class.getName());
-    private String host;
+    private Logger logger = Logger.getLogger(RDFWriter.class.getName());
+    private Document tags = null;
 
-    public RDFWriter() {
+    public RDFWriter() throws FileNotFoundException, IOException {
+        FileReader reader = new FileReader("/home/williams/projetos/osmosis/osmosis-rdf/conf/tags.json");
+        BufferedReader buffer = new BufferedReader(reader);
+        StringBuilder text_tags = new StringBuilder();
+        String line;
+        while ((line = buffer.readLine()) != null) {
+            text_tags.append(line);
+        }
+        buffer.close();
+        reader.close();
 
+        tags = Document.parse(text_tags.toString());
+        System.out.println("oioioio");
     }
 
-
+    @Override
     public void process(EntityContainer entityContainer) {
 
         Entity entity = entityContainer.getEntity();
-        EntityType type = entity.getType();
 
-        if(entity.getType().equals(EntityType.Node)){
+        if (entity.getType().equals(EntityType.Node)) {
             Node node = (Node) entity;
-
             TagCollection tagCollection = (TagCollection) node.getTags();
             Map<String, Object> map = new HashMap<>();
+            boolean status = false;
+            String key = null;
+            String value = null;
+            Map<String, String> buildMap = tagCollection.buildMap();
+            for (Map.Entry<String, String> entry : buildMap.entrySet()) {
+                String k = entry.getKey();
+                String v = entry.getValue();
+                List<String> list = tags.get(k, List.class);
+                if (list.contains(v)) {
+                    status = true;
+                    key = k;
+                    value = v;
+                }
+            }
+            if (key != null) {
+                buildMap.remove(key);
 
-            tagCollection.buildMap().forEach((k,v) ->{
-                //System.out.println(k + " - " + v); //created_by - JOSM
-                //System.out.println(entity.toString() + "\n\n"); //Node(id=94754991, #tags=1)
-                map.put(k.replace(".","_"),v);
-            });
+                String[] value_split = value.split("[_-]");
+                StringBuilder value_URI = new StringBuilder();
+                value_URI.append(OSM);
+                for (int i = 0; i < value_split.length; i++) {
+                    value_URI.append(WordUtils.capitalizeFully(value_split[i]));
+                }
 
+                URI resource = URI.create(value_URI.toString() + "/" + entity.getId());
+                map.put(OSM + "hasOSMID", entity.getId());
+                map.put(RDF + "type", URI.create(value_URI.toString()));
+
+                for (Map.Entry<String, String> entry : buildMap.entrySet()) {
+                    String k = entry.getKey();
+                    String v = entry.getValue();
+                    switch (k) {
+                        case "name":
+                            map.put(OSM + "hasName", v);
+                            break;
+                        default:
+                            map.put(k.replace(".", "_"), v);
+                    }
+                }
+
+            }
 
             //Point point = new Point(new Position(node.getLongitude(),node.getLatitude()));
-            //map.put("loc",point);
-
-            //metadata
-            map.put("_id",entity.getId());
-            map.put("version",entity.getVersion());
-            map.put("timestamps",entity.getTimestamp().getTime());
-
-//            System.out.println(map.toString());
-            if(map.containsKey("name")){
-                System.out.println(map.get("name"));
-            }
-            if(map.containsKey("value")){
-                System.out.println(map.get("value"));
-                System.out.println("\n");
-            }
-
-
-        }else{
+            //map.put("loc",point);            
+        } else {
             System.out.println("--------");
             System.out.println(Node.class.getSimpleName());
             System.out.println(entity.getType().name());
         }
     }
 
+    @Override
     public void complete() {
 
     }
 
-    public void initialize(Map<String, Object> metaData) {
+    @Override
+    public void initialize(Map<String, Object> metaData
+    ) {
+        System.out.println("metadata: " + metaData.toString());
         logger.fine("initialize() with metadata: " + metaData.toString());
     }
 
+    @Override
     public void close() {
 
     }
--- osmosis-rdf/src/main/java/org/openstreetmap/osmosis/rdf/RDFWriterFactory.java
+++ osmosis-rdf/src/main/java/org/openstreetmap/osmosis/rdf/RDFWriterFactory.java
@@ -1,36 +1,30 @@
 package org.openstreetmap.osmosis.rdf;
 
-import org.openstreetmap.osmosis.core.pipeline.common.RunnableTaskManager;
 import org.openstreetmap.osmosis.core.pipeline.common.TaskConfiguration;
 import org.openstreetmap.osmosis.core.pipeline.common.TaskManager;
 import org.openstreetmap.osmosis.core.pipeline.common.TaskManagerFactory;
 import org.openstreetmap.osmosis.core.pipeline.v0_6.SinkManager;
 import org.openstreetmap.osmosis.core.task.v0_6.Sink;
-
-import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by williams on 27/01/17.
  */
 public class RDFWriterFactory extends TaskManagerFactory {
+
+    @Override
     protected TaskManager createTaskManagerImpl(TaskConfiguration taskConfig) {
-        Sink sink = null;
-        System.out.println("---------");
-        System.out.println(taskConfig.getConfigArgs());
-        System.out.println(taskConfig.getPipeArgs());
-        System.out.println(taskConfig.getDefaultArg());
 
-        Map<String, String> configArgs = taskConfig.getConfigArgs();
-
-        try{
-            sink= new RDFWriter();
-        }catch (Exception e){
+        SinkManager sinkManager = null;
+        try {
+            Sink sink = new RDFWriter();
+            sinkManager = new SinkManager(taskConfig.getId(), sink, taskConfig.getPipeArgs());
+        } catch (Exception e) {
             System.out.println(taskConfig.getDefaultArg());
             System.out.println(taskConfig.getPipeArgs());
             System.out.println(taskConfig.getConfigArgs());
         }
-        return new SinkManager(taskConfig.getId(),sink,taskConfig.getPipeArgs());
+        return sinkManager;
     }
 
 }
