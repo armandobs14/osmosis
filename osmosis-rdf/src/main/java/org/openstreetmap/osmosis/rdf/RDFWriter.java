3a26d9e0140b63f4f7e0a496233cd789f0158f0d
williamsla <williamsalcantara@gmail.com>
2017-02-01 01:29:25 -0300

    removing the call for address service to avoid revoke connection status

--- osmosis-rdf/src/main/java/org/openstreetmap/osmosis/rdf/RDFWriter.java
+++ osmosis-rdf/src/main/java/org/openstreetmap/osmosis/rdf/RDFWriter.java
@@ -6,7 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer;
 import org.openstreetmap.osmosis.core.domain.v0_6.*;
 import org.openstreetmap.osmosis.core.task.v0_6.Sink;
@@ -16,9 +15,7 @@ import java.util.List;
 import java.util.logging.Level;
 import org.bson.Document;
 import org.apache.commons.lang3.text.WordUtils;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
@@ -35,7 +32,6 @@ public class RDFWriter implements Sink {
 
     private File file = null;
     private final FileWriter fileWriter;
-    private HttpClient client;
 
     public RDFWriter() throws FileNotFoundException, IOException {
         FileReader reader = new FileReader("/home/williams/projetos/osmosis/osmosis-rdf/conf/tags.json");
@@ -50,10 +46,8 @@ public class RDFWriter implements Sink {
 
         tags = Document.parse(text_tags.toString());
 
-        file = new File("/home/williams/projetos/dados/osm.n4");
+        file = new File("/home/williams/projetos/dados/osm.nt");
         fileWriter = new FileWriter(file, true);
-
-        client = new DefaultHttpClient();
     }
 
     @Override
@@ -82,22 +76,6 @@ public class RDFWriter implements Sink {
             if (key != null) {
                 buildMap.remove(key);
                 try {
-                    String url = "http://api.boamoradia.com.br:8080/address?lat=" + node.getLatitude() + "&lon=" + node.getLongitude() + "&datatype=Building";
-
-                    HttpGet request = new HttpGet(url);
-                    HttpResponse response = client.execute(request);
-
-                    BufferedReader rd = new BufferedReader(
-                            new InputStreamReader(response.getEntity().getContent()));
-
-                    StringBuilder result = new StringBuilder();
-                    String line = "";
-                    while ((line = rd.readLine()) != null) {
-                        result.append(line);
-                    }
-                    Document adr = Document.parse(result.toString());
-                    String dataset = "<http://linkn.com.br/data/building/br/" + adr.getString("state_uf").toLowerCase() + "/>";
-
                     String[] value_split = value.split("[_-]");
                     StringBuilder value_URI = new StringBuilder();
                     value_URI.append(OSM);
@@ -106,23 +84,20 @@ public class RDFWriter implements Sink {
                     }
 
                     String resource = "<" + value_URI.toString() + "/" + entity.getId() + ">";
+                    String adr_URI = "<" + OSM + "address/" + entity.getId() + ">";
 
                     fileWriter.append(resource)
                             .append(" <" + RDF + "type> ")
                             .append(" <" + value_URI.toString() + "> ")
-                            .append(dataset)
                             .append(".\n");
                     fileWriter.append(resource)
                             .append(" <" + OSM + "hasOSMID> ")
                             .append(" \"" + String.valueOf(entity.getId()) + "\"^^xsd:int ")
-                            .append(dataset)
                             .append(".\n");
                     fileWriter.append(resource)
                             .append(" <" + LOC + "hasAddress> ")
-                            .append(" <" + adr.getString("uri") + "> ")
-                            .append(dataset)
+                            .append(adr_URI)
                             .append(".\n");
-
                     for (Map.Entry<String, String> entry : buildMap.entrySet()) {
                         String k = entry.getKey();
                         String v = entry.getValue();
@@ -131,19 +106,29 @@ public class RDFWriter implements Sink {
                                 fileWriter.append(resource)
                                         .append(" <" + OSM + "hasName> ")
                                         .append(" \"" + v + "\"^^xsd:string ")
-                                        .append(dataset)
                                         .append(".\n");
                                 break;
                             default:
                         }
                     }
-                } catch (IOException | org.bson.json.JsonParseException ex) {
+                    fileWriter.append(adr_URI)
+                            .append(" <" + RDF + "type> ")
+                            .append(" <" + LOC + "Address> ")
+                            .append(".\n");
+                    fileWriter.append(adr_URI)
+                            .append(" <" + LOC + "hasLatitude> ")
+                            .append(" \"" + node.getLatitude() + "\"^^xsd:string ")
+                            .append(".\n");
+                    fileWriter.append(adr_URI)
+                            .append(" <" + LOC + "hasLongitude> ")
+                            .append(" \"" + node.getLongitude() + "\"^^xsd:string ")
+                            .append(".\n");
+                } catch (IOException ex) {
 
                 }
             }
         } else {
-            System.out.println("--------");
-            System.out.println(Node.class.getSimpleName());
+            System.out.println("--------");            
             System.out.println(entity.getType().name());
         }
     }
